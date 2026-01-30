cb862b0def3d3ded3770e6990e157d449deade08
devamanas-t <devamanasthenari@gmail.com>
2026-01-30 10:13:46 +0530

    Create databse.py
    Initial commit bug fix

---
+++ databse.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# UPDATE THIS LINE with your MySQL password and database name
+# Format: mysql+pymysql://username:password@localhost/databasename
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:password@localhost/dshare_db"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
